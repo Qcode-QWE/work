8472e2eeba7cbcdefb57c0f05b1d608ee1062ff1
Qcode-QWE <1157026826@qq.com>
2019-01-07 21:02:30 +0800

    商品详情分支

---
+++ orangelala/src/main/java/com/orangelala/controller/IntroductionController.java
@@ -0,0 +1,15 @@
+package com.orangelala.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**  
+* <p>Title: IntroductionController.java</p>  
+* <p>Description:商品详情controller </p>   
+* @author QEcode  
+* @date 2019年1月7日  
+* @version 1.0  
+*/ 
+@Controller
+public class IntroductionController {
+
+}
